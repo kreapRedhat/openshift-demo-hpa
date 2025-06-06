7cb4a4d96f0728d61da293ac9d0f595b046053d4
kreap <kreap@redhat.com>
2025-06-06 09:02:18 -0500

    Updating hpa demo to correct project namespace

--- openshift-hpa-install.sh
+++ openshift-hpa-install.sh
@@ -1,10 +1,10 @@
-oc new-project hpa-project
-oc apply -f manifests/backend-v1.yaml -n hpa-project
-oc apply -f manifests/backend-service.yaml -n hpa-project
-oc apply -f manifests/backend-route.yaml -n hpa-project
-oc set env deployment backend-v1 -n hpa-project APP_BACKEND=https://httpbin.org/status/200
-oc wait --for=condition=ready --timeout=60s pod -l app=backend
-BACKEND_URL=http://$(oc get route backend -n hpa-project -o jsonpath='{.spec.host}')
+oc new-project project1
+oc apply -f manifests/backend-v1.yaml -n project1
+oc apply -f manifests/backend-service.yaml -n project1
+oc apply -f manifests/backend-route.yaml -n project1
+oc set env deployment backend-v1 -n project1 APP_BACKEND=https://httpbin.org/status/200
+oc wait --for=condition=ready --timeout=60s pod -l app=backend -n project1
+BACKEND_URL=http://$(oc get route backend -n project1 -o jsonpath='{.spec.host}')
 curl -v -k $BACKEND_URL
-oc apply -f manifests/backend-cpu-hpa.yaml -n hpa-project
-#watch oc get horizontalpodautoscaler/backend-cpu -n hpa-project
+oc apply -f manifests/backend-cpu-hpa.yaml -n project1
+#watch oc get horizontalpodautoscaler/backend-cpu -n project1
